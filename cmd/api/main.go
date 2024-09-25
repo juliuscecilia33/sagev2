53070e26334fc22acb4c68b01e889911c8b5192e
Julius <juliuscecilia33@gmail.com>
2024-09-24 18:05:40 -0700

    database and config file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/juliuscecilia33/sagev2/config"
+	"github.com/juliuscecilia33/sagev2/db"
 	"github.com/juliuscecilia33/sagev2/handlers"
 	"github.com/juliuscecilia33/sagev2/repositories"
 )
 
 func main() {
+	envConfig := config.NewEnvConfig() // Load config from environment variables
+	db := db.Init(envConfig)
+
 	app := fiber.New(fiber.Config{
 		AppName: "Sage",
 		ServerHeader: "Fiber",
