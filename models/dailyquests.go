57d354d058cf8d08f01f340209b4ffea3832a541
Julius <juliuscecilia33@gmail.com>
2024-11-05 20:48:22 -0800

    daily quests models and repositories

---
+++ models/dailyquests.go
@@ -0,0 +1,29 @@
+package models
+
+import (
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/juliuscecilia33/sagev2/utils"
+)
+
+type DailyQuest struct {
+	ID 					uuid.UUID			`json:"id" gorm:"type:uuid;primaryKey;default:uuid_generate_v4()"`
+	RewardID			uuid.UUID			`json:"rewardId" gorm:"type:uuid;not null;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;references:ID"`
+	Name				string				`json:"name" gorm:"type:text; not null"`
+	Description			string				`json:"description" gorm:"type:text"`
+	Requirements		utils.NestedJSONMap	`json:"requirements" gorm:"type:jsonb; not null"`
+	Type				string				`json:"type" gorm:"type:text"`
+	Reward      		Reward          	`gorm:"foreignKey:RewardID;references:ID"`
+	CreatedAt 			time.Time			`json:"created_at"`
+	UpdatedAt 			time.Time			`json:"udpated_at"`
+}
+
+type DailyQuestRepository interface {
+	GetMany(ctx context.Context) ([]*DailyQuest, error)
+	GetOne(ctx context.Context, dailyQuestId uuid.UUID) (*DailyQuest, error)
+	CreateOne(ctx context.Context, dailyQuest *DailyQuest) (*DailyQuest, error)
+	UpdateOne(ctx context.Context, dailyQuestId uuid.UUID, updateData map[string]interface{}) (*DailyQuest, error)
+	DeleteOne(ctx context.Context, dailyQuestId uuid.UUID) error
+}
---
+++ repositories/dailyquests.go
@@ -0,0 +1,82 @@
+package repositories
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/juliuscecilia33/sagev2/models"
+	"gorm.io/gorm"
+)
+
+type DailyQuestRepository struct {
+	db *gorm.DB
+}
+
+func (r *DailyQuestRepository) GetMany(ctx context.Context) ([]*models.DailyQuest, error) {
+	dailyQuests := []*models.DailyQuest{}
+
+	res := r.db.Model(&models.DailyQuest{}).Preload("Reward").Order("updated_at desc").Find(&dailyQuests)
+
+	if res.Error != nil {
+		return nil, res.Error
+	}
+
+	return dailyQuests, nil
+}
+
+func (r *DailyQuestRepository) GetOne(ctx context.Context, dailyQuestId uuid.UUID) (*models.DailyQuest, error) {
+	dailyQuest := &models.DailyQuest{}
+
+	res := r.db.Model(dailyQuest).Preload("Reward").Where("id = ?", dailyQuestId).First(dailyQuest)
+
+	if res.Error != nil {
+		return nil, res.Error
+	}
+
+	return dailyQuest, nil
+}
+
+func (r *DailyQuestRepository) CreateOne(ctx context.Context, dailyQuest *models.DailyQuest) (*models.DailyQuest, error) {
+	res := r.db.Create(dailyQuest)
+
+	if res.Error != nil {
+		return nil, res.Error
+	}
+
+	if err := r.db.Preload("Reward").First(dailyQuest, dailyQuest.ID).Error; err != nil {
+		return nil, err
+	}
+
+	return dailyQuest, nil
+}
+
+func (r DailyQuestRepository) UpdateOne(ctx context.Context, dailyQuestId uuid.UUID, updateData map[string]interface{}) (*models.DailyQuest, error) {
+	dailyQuest := &models.DailyQuest{}
+
+	updateRes := r.db.Model(dailyQuest).Where("id = ?", dailyQuestId).Updates(updateData)
+
+	if updateRes.Error != nil {
+		return nil, updateRes.Error
+	}
+
+	getRes := r.db.Where("id = ?", dailyQuestId).First(dailyQuest)
+
+	if getRes.Error != nil {
+		return nil, getRes.Error
+	}
+
+	return dailyQuest, nil
+}
+
+func (r *DailyQuestRepository) DeleteOne(ctx context.Context, dailyQuestId uuid.UUID) error {
+	res := r.db.Delete(&models.DailyQuest{}, dailyQuestId)
+
+	return res.Error
+}
+
+
+func NewDailyQuestRepository(db *gorm.DB) models.DailyQuestRepository {
+	return &DailyQuestRepository{
+		db: db,
+	}
+}
