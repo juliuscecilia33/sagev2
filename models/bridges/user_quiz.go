ce7c52d2fba586c494047c05e378385b64357ac9
Julius <juliuscecilia33@gmail.com>
2024-11-07 12:27:40 -0800

    finished user quiz table

--- models/bridges/user_quiz.go
+++ models/bridges/user_quiz.go
@@ -13,14 +13,14 @@ type UserQuiz struct {
 	ID 				uuid.UUID     		`json:"id" gorm:"type:uuid;primaryKey;default:uuid_generate_v4()"`
 	UserID          uuid.UUID     		`json:"userId" gorm:"type:uuid;not null;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;references:ID"`
 	QuizID         	uuid.UUID     		`json:"quizId" gorm:"type:uuid;not null;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;references:ID"`
-	Progress		utils.NestedJSONMap `json:"requirements" gorm:"type:jsonb"`
-	Score			int              `json:"score" gorm:"type:int"`
+	Progress		utils.NestedJSONMap `json:"progress" gorm:"type:jsonb"`
+	Score			int              	`json:"score" gorm:"type:int"`
 	StartedAt   	time.Time           `json:"started_at" gorm:"type:timestamp"` // Timestamp for when the quiz started
     CompletedAt 	time.Time           `json:"completed_at" gorm:"type:timestamp"` // Timestamp for when the quiz was completed
 	CreatedAt   	time.Time			`json:"created_at"`
 	UpdatedAt   	time.Time			`json:"udpated_at"`
-	User            models.User          		`gorm:"foreignKey:UserID;references:ID"`
-	Quiz          	models.Quiz       	  		`gorm:"foreignKey:QuizID;references:ID"`
+	User            models.User          `gorm:"foreignKey:UserID;references:ID"`
+	Quiz          	models.Quiz       	  `gorm:"foreignKey:QuizID;references:ID"`
 }
 
 type UserQuizRepository interface {
